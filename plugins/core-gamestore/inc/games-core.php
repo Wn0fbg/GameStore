9c4bca6cfe93e255b601e8f8e510d67c9526f948
Wn0fbg <temakozlovofficiall@gmail.com>
2026-07-29 12:39:12 +0300

    start search popup

---
+++ plugins/core-gamestore/inc/games-core.php
@@ -0,0 +1,22 @@
+<?php
+
+function gamestore_footer_search_popup() {
+    ?>
+    <div class="popup-games-search-container">
+        <span id="close-search"></span>
+        <div class="search-container">
+            <div class="searc-bar wrapper">
+                <h3 class="search-label">Search</h3>
+                <input 
+                  type="text" 
+                  name="game-title" 
+                  id="popup-search-input" 
+                  placeholder="Search for Games"
+                />
+            </div>
+        </div>
+    </div>
+    <?php
+}
+
+add_action("wp_footer","gamestore_footer_search_popup");
