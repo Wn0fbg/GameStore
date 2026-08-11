9f38b6ff0e93a61f6c7b776eb4353c0ba6c7beba
Wn0fbg <temakozlovofficiall@gmail.com>
2026-08-11 23:22:28 +0300

    start adaptive for header

--- plugins/blocks-gamestore/build/blocks-gamestore/block-header/index.asset.php
+++ plugins/blocks-gamestore/build/blocks-gamestore/block-header/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'a3e491f82f2303d0c719');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '0359bfbec1e03a209da6');
